7c65d979487960e21e499d2a500ce264770dde4a
marquin.e <marcos.emanuel.04@gmail.com>
2022-10-29 21:12:08 -0300

    Agora ele abre o explorador de arquivos depois que baixar o vídeo

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
 # Importando bibliotecas
 from pytube import YouTube
-from os import path, mkdir
+import os
+from time import sleep
 
 # Função para verificar se o caminho onde será salvo o vídeo existe. Caso não exista, será criado.
 def verificaCaminho(caminho):
-    existeCaminho = path.exists(caminho)
+    existeCaminho = os.path.exists(caminho)
     if existeCaminho != True:
-        mkdir(caminho)
+        os.mkdir(caminho)
 
 # Receber URL do vídeo e recebendo o caminho para salvar o arquivo baixado
 link = input("Cole aqui a URL do vídeo que deseja baixar: ")
@@ -25,3 +26,9 @@ ys = yt.streams.get_highest_resolution()
 print("Baixando...")
 ys.download(caminho)
 print("Video baixado com sucesso!")
+
+# Abrindo a pasta onde foi salvo o vídeo
+for i in range(1, 4):
+    print(f'Abrindo diretório em {i}...')
+    sleep(1)
+os.startfile(caminho)
