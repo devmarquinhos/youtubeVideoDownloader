a1c5729c61851b7349f41751750119bdf092ba0d
marquin.e <marcos.emanuel.04@gmail.com>
2023-01-12 15:36:26 -0300

    Funções melhoradas e título em ASCII criado

--- index.py
+++ index.py
@@ -2,18 +2,31 @@
 from pytube import YouTube
 import os
 from time import sleep
+import pyfiglet
 
+# Função para criar o título
+def titleAscii():
+    titleFinal = pyfiglet.figlet_format("YouTube Downloader", font = "big")
+    os.system('cls')
+    print("="*50)
+    print(titleFinal)
+    print("="*50)
 
 # Função para verificar se o caminho onde será salvo o vídeo existe. Caso não exista, será criado.
 def verificaCaminho(caminho):
     existeCaminho = os.path.exists(caminho)
     if not existeCaminho:
         os.mkdir(caminho)
+        
+# Função para abrir a pasta
+def openDir(caminho):
+    for i in range(1, 4):
+        print(f'Abrindo diretório em {i}...')
+        sleep(1)
+    os.startfile(caminho)
 
-os.system('cls')
-print("="*30)
-print("Youtube Downloader | v1.0")
-print("="*30)
+# Imprimindo o título
+titleAscii()
 
 # Receber URL do vídeo e recebendo o caminho para salvar o arquivo baixado
 link = input("Cole aqui a URL do vídeo que deseja baixar: ")
@@ -34,7 +47,4 @@ ys.download(caminho)
 print("Video baixado com sucesso!")
 
 # Abrindo a pasta onde foi salvo o vídeo
-for i in range(1, 4):
-    print(f'Abrindo diretório em {i}...')
-    sleep(1)
-os.startfile(caminho)
+openDir(caminho)
